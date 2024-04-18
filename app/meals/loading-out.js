6184f179db2f11689b6aa3da9583cd4253ef635e
Hannah Z <eveline1129@163.com>
2024-04-17 19:38:50 -0500

    use Suspense for loading instead

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,4 +1,4 @@
-import classes from "./loading.module.css"
+import classes from "./loading-out.module.css"
 export default function MealsLoadingPage() {
 	return <p className={classes.loading}>Fetching meals</p>
 }

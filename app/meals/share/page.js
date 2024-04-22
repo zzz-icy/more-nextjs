2aa1f4df35f8c905c19da418f3dd4b6266a220c5
Hannah Z <eveline1129@163.com>
2024-04-21 23:21:52 -0500

    added MealsFormSubmit and redirect UX

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker"
 import classes from "./page.module.css"
 import { shareMeal } from "@/lib/actions"
+import MealsFormSubmit from "@/components/meals/meals-form-submit"
 
 export default function ShareMealPage() {
 	return (
@@ -68,7 +69,7 @@ export default function ShareMealPage() {
 						name='image'
 					/>
 					<p className={classes.actions}>
-						<button type='submit'>Share Meal</button>
+						<MealsFormSubmit />
 					</p>
 				</form>
 			</main>

5c35b5391f54a968ed8ee373b74c19be4fd266fd
Hannah Z <eveline1129@163.com>
2024-04-23 22:00:12 -0500

    added more meals for testing purpose and added dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,17 @@ import classes from "./page.module.css"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 
+export async function generateMetadata({ params }) {
+	const meal = getMeal(params.mealSlug)
+	if (!meal) {
+		notFound()
+	}
+	return {
+		title: meal.title,
+		description: meal.summary,
+	}
+}
+
 export default function MealDetailsPage({ params }) {
 	const meal = getMeal(params.mealSlug)
 	// the notFound function, which is imported from next/navigation.

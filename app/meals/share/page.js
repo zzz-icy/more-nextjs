152078a3c95d7aa1af7de646891eead37f6708c3
Hannah Z <eveline1129@163.com>
2024-04-23 21:13:48 -0500

    useFormState for sharing a meal error handling

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,11 @@
+"use client"
 import ImagePicker from "@/components/meals/image-picker"
 import classes from "./page.module.css"
 import { shareMeal } from "@/lib/actions"
 import MealsFormSubmit from "@/components/meals/meals-form-submit"
-
+import { useFormState } from "react-dom"
 export default function ShareMealPage() {
+	const [state, formAction] = useFormState(shareMeal, { message: null })
 	return (
 		<>
 			<header className={classes.header}>
@@ -15,7 +17,7 @@ export default function ShareMealPage() {
 			<main className={classes.main}>
 				<form
 					className={classes.form}
-					action={shareMeal}
+					action={formAction}
 				>
 					<div className={classes.row}>
 						<p>
@@ -68,6 +70,7 @@ export default function ShareMealPage() {
 						label={"your image"}
 						name='image'
 					/>
+					{state.message && <p>{state.message}</p>}
 					<p className={classes.actions}>
 						<MealsFormSubmit />
 					</p>
